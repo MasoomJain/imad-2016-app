3703d7483b041370be9c67d0b04708ecb8f9be5d
MasoomJain <masoombakliwal@gmail.com>
2016-09-29 22:13:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,22 @@ button.onclick = function (){
     request.open('GET','http://masoomjain.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var nameInput = getElementById('name');
+var name = nameInput.value;
+var submit = getElementById('submit_btn');
+submit.onclick = function () {
+    
+    
+    
+    var names = ['name1','name2','name2'];
+    var list = '';
+    for (var i=0; i<names.length ; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+    
+};

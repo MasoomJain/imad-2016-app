a62cfeaa5c8aa5332dbf9b230b88c60488fc4175
MasoomJain <masoombakliwal@gmail.com>
2016-09-29 20:06:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,11 @@
 //Counter Code
 var button = document.getElementById('counter');
+var counter = 0; 
 
 button.onclick = function (){
     
-    // create a request to the counter endpoint
-    var request = new XMLHttpRequest(); 
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
     
-    
-    //capture the response and store it in a variable
-    request.onreadystateschange = function () {
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter  = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        
-    };
-  // make a request
-  request.open('GET','http://masoomjain.imad.hasura-app.io/counter',true);
-  request.send(null);
 };

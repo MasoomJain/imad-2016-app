a7439f32a0395ad0f146b9c89f98633b4f5c58bb
MasoomJain <masoombakliwal@gmail.com>
2016-09-29 19:00:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = "How you Doing ?";
 
-var img = document.getElementById('madi');
+var img = document.getElementById('me');
 var marginLeft = 0;
 function moveRight () {
      marginLeft = marginLeft + 1;

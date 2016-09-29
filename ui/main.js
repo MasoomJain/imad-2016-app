23cf13864f4ed820d422d3bec5a62f5c2a9c4610
MasoomJain <masoombakliwal@gmail.com>
2016-09-29 18:31:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text");
 element.innerHTML = "How you Doing ?";
+
+var img = document.getElementById("madi")
+img.onclick = function () {
+   img.style.marginLeft = '100px' ;   
+};
